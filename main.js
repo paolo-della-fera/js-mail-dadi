9085773a74df8fb181a36512c4381235b4819df1
paolo-della-fera <paolodf2005@gmail.com>
2026-02-05 14:22:06 +0100

    add array with the emails of the invited users

---
+++ main.js
@@ -0,0 +1,8 @@
+// inizio creandomi una lista di email delle persone invitate alla festa 
+const emails = [
+    "luca.rossi@email.com",
+    "maria.bianchi@testmail.net",
+    "giovanni.verdi@mailbox.org",
+    "anna.neri@fakepost.it",
+    "paolo.gialli@demoemail.com"
+]
