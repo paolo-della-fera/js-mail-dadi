ba62e4fd96d516fdc09c23408edd4f037a69f544
paolo-della-fera <paolodf2005@gmail.com>
2026-02-05 15:13:32 +0100

    add email validation and access control logic

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// MAIL
+
 // inizio creandomi una lista di email delle persone invitate alla festa 
 const emails = [
     "luca.rossi@email.com",
@@ -11,4 +13,23 @@ const emails = [
 let email = (prompt('inserisci la tua email'))
 
 // mi creo una variabile dove salvare l'email se la troviamo nella lista
-let emailFound = false 
+let emailFound = false
+
+// creo il ciclo for per controllare se l'email inserita è all'interno dell'elenco degli invitati 
+for (let i = 0; i < emails.length; i++) {
+
+    // SE l'email è all'interno della lista viene salvato all'interno della variabile emailFound
+    if (email === emails[i]) {
+        emailFound = true;
+    }
+}
+
+// SE l'email inserita è presente nell'elenco viene consentito l'accesso 
+if (emailFound) {
+    console.log('Accesso consentito ✅');
+}
+
+//ALTRIMENTI se l'email insertita non è presente viene negato l'accesso
+else {
+    console.log('Accesso negato ❌');
+}
